57b1b3b7b514c85ad9f6b3dcd6e9d41dee74df95
Sean Caulfield <sean@yak.net>
2016-03-12 21:24:20 -0500

    new hardware, 4 dig display

--- thermometer.h
+++ thermometer.h
@@ -15,43 +15,8 @@
 // MACRO (POLO)
 ////////////////////////////////////////////////////////////////////////
 
-// What unit to use for display?
-#define UNIT_C 0
-#define UNIT_F 1
-#define UNIT_K 2
-#define USE_UNIT UNIT_F
-
-// Only really used for serial connection
-#if   USE_UNIT == UNIT_C
-#define UNIT_SYMBOL F("C")
-#elif USE_UNIT == UNIT_F
-#define UNIT_SYMBOL F("F")
-#elif USE_UNIT == UNIT_K
-#define UNIT_SYMBOL F("K")
-#else
-#error "USE_UNIT not defined properly!"
-#endif
-
-// Fixed precision divider
-#define THERM_DIVIDER 16.0
-
 // How often to poll for temperature, in milliseconds?
 #define THERM_POLL_INTERVAL 1000
-
-#define INT2CHAR(x) ((char)((x) + 0x30))
-
-// Seven segment pin mappings
-#define PIN_SEG_A 3
-#define PIN_SEG_B 7
-#define PIN_SEG_C A0
-#define PIN_SEG_D A2
-#define PIN_SEG_E A3
-#define PIN_SEG_F 4
-#define PIN_SEG_G 13
-#define PIN_SEG_P A1
-#define PIN_DIG_1 2
-#define PIN_DIG_2 5
-#define PIN_DIG_3 6
 
 ////////////////////////////////////////////////////////////////////////
 // DEBUGGING
--- thermometer.ino
+++ thermometer.ino
@@ -21,26 +21,21 @@
 // Temperature sensor
 Adafruit_MCP9808 therm = Adafruit_MCP9808();
 uint32_t last_check = 0;
-float avg_temp = 0.0;
-int16_t last_temp = 0;
+float last_temp = 0.0;
 boolean sensor_error = false;
-
-const size_t HISTORY_LENGTH = 4;
-int16_t temp_history[HISTORY_LENGTH];
-size_t temp_history_pos = 0;
-size_t temp_history_max = 0;
 
 // Display
 SevSeg display;
 
-const size_t DISP_DIGITS = 3;
+const size_t DISP_DIGITS = 4;
 const int DISP_ON_TIME_USEC = 100;
 const int DISP_OFF_TIME_USEC = 1000;
 
 const byte PINS_DIGITS[DISP_DIGITS] = {
   2, // digit 1 (leftmost)
   5, // digit 2
-  6, // digit 3 (rightmost)
+  6, // digit 3
+  12, // digit 4 (rightmost)
 };
 
 const byte PINS_SEGMENTS[] = {
@@ -51,67 +46,35 @@ const byte PINS_SEGMENTS[] = {
   A3, // segment e
   4,  // segment f
   13, // segment g
-  A1, // decimal point
+  A1, // decimal point <-- doesn't actually work with HS420561K-C30
 };
 
 ////////////////////////////////////////////////////////////////////////
 // HALPING
 ////////////////////////////////////////////////////////////////////////
 
-void recordTemp(int16_t temp) {
-  //DPRINT(F("temp_history_pos1=")); DPRINTLN(temp_history_pos);
-  //DPRINT(F("temp_history_max1=")); DPRINTLN(temp_history_max);
-  temp_history[temp_history_pos] = temp;
-  temp_history_pos = (temp_history_pos + 1) % HISTORY_LENGTH;
-  if (temp_history_max < HISTORY_LENGTH) {
-    temp_history_max++;
-  }
-  //DPRINT(F("temp_history_pos2=")); DPRINTLN(temp_history_pos);
-  //DPRINT(F("temp_history_max2=")); DPRINTLN(temp_history_max);
-}
-
-float readRollingAvgTemp() {
-
-  int16_t total = 0;
-  for (size_t i=0; i<temp_history_max; i++) {
-    total += temp_history[i];
-  }
-  total /= temp_history_max;
-  //DPRINT(F("total=")); DPRINTLN(total);
-
-  float temp = total / THERM_DIVIDER;
-  //DPRINT(F("temp1=")); DPRINTLN(temp);
-
-  // Peform temp conversion but only compile in code as required
-#if   USE_UNIT == UNIT_C
-  // Do nothing because the default is C
-#elif USE_UNIT == UNIT_F
-  temp = temp * 9.0 / 5.0 + 32;
-#elif USE_UNIT == UNIT_K
-  temp = temp + 273.15;
-#else
-#error "UNIT NOT DEFINED!"
-#endif
-
-  //DPRINT("temp="); DPRINTLN(temp);
-
-  return temp;
-
-}
-
-int16_t readTemp() {
-  int16_t temp;
+float readTemp() {
   
   // Return error condition on sensor failure
   if (sensor_error) {
     return 999;
   }
 
+  float temp;
+
   //therm.shutdown_wake(0);
-  temp = therm.readTempRaw();
+  temp = therm.readTempC();
   //therm.shutdown_wake(1);
 
+
+  // Convert to Fahrenheit because I suck and can't read C well (yet)
+  temp = temp * 9.0 / 5.0 + 32;
+
+  DPRINT( "tempC="); DPRINT(temp);
+  DPRINT(" tempF="); DPRINTLN(temp);
+
   return temp;
+
 }
 
 uint32_t time_diff(uint32_t a, uint32_t b) {
@@ -161,50 +124,26 @@ void loop() {
   if (time_diff(last_check, now) > THERM_POLL_INTERVAL) {
     last_check = now;
     last_temp = readTemp();
-    //DPRINT(F("last_temp=")); DPRINT(last_temp);
-    recordTemp(last_temp);
-    avg_temp = readRollingAvgTemp();
-    DPRINT(F(" avg_temp=")); DPRINTLN(avg_temp);
   }
 
-  //char dig1, dig2, dig3;
   int8_t dp = 0; // # after . to show
-  if (avg_temp <= -10 && avg_temp > -100) {
-    // dig1 = '-';
-    // dig2 = INT2CHAR(int(avg_temp) / 10);
-    // dig3 = INT2CHAR(int(avg_temp) % 10);
+  if (last_temp <= -10 && last_temp > -100) {
     dp = 1;
-  } else if (avg_temp < 0 && avg_temp > -10) {
-    // dig1 = '-';
-    // dig2 = INT2CHAR(int(avg_temp) % 10);
-    // dig3 = INT2CHAR(int(avg_temp * 10) % 10);
+  } else if (last_temp < 0 && last_temp > -10) {
     dp = 1;
-  } else if (avg_temp >= 0 && avg_temp < 10) {
-    // dig1 = INT2CHAR(int(avg_temp) % 10);
-    // dig2 = INT2CHAR(int(avg_temp * 10) % 10);
-    // dig2 = INT2CHAR(int(avg_temp * 100) % 10);
+  } else if (last_temp >= 0 && last_temp < 10) {
     dp = 1;
-  } else if (avg_temp >= 10 && avg_temp < 100) {
-    // dig1 = INT2CHAR(int(avg_temp) / 10);
-    // dig2 = INT2CHAR(int(avg_temp) % 10);
-    // dig3 = INT2CHAR(int(avg_temp * 10) % 10);
+  } else if (last_temp >= 10 && last_temp < 100) {
     dp = 1;
-  } else if (avg_temp >= 100 && avg_temp < 1000) {
-    // dig1 = INT2CHAR(int(avg_temp) / 100);
-    // dig2 = INT2CHAR(int(avg_temp) / 10);
-    // dig3 = INT2CHAR(int(avg_temp) % 10);
+  } else if (last_temp >= 100 && last_temp < 1000) {
     dp = 0;
   } else { // error state
-    // dig1 = // dig2 = // dig3 = 'E';
     dp = -1;
   }
 
-  //DPRINT(dig1);
-  //DPRINT(dig2);
-  //DPRINTLN(dig3);
   if (dp > 0) {
-    //display.setNumber(avg_temp, dp);
-    display.setNumber(999, 0);
+    display.setNumber(last_temp, dp);
+    //display.setNumber(999, 0);
   } else {
     display.setNumber(999, 0);
   }
